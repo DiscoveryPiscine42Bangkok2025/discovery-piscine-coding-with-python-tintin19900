4d84f0130d17987345b19a10f175fd96005068c8
PeerawitTin <119733857+tintin19900@users.noreply.github.com>
2025-09-18 13:09:42 +0700

    Update downcase_all.py

--- cell06/ex02/downcase_all.py
+++ cell06/ex02/downcase_all.py
@@ -3,8 +3,9 @@ import sys
 def downcase_all(str) :
     return str.lower()
 
-if len(sys.argv) > 2 :
+if len(sys.argv) >= 2 :
     for str in sys.argv[1:] :
         print(downcase_all(str))
 else :
+
     print("none")
